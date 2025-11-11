988fc62d276924e34c35afe863d2ba0845d2af91
saif <seif301x@gmail.com>
2025-11-11 20:37:09 +0200

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,52 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.blue, borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 24, bottom: 24, top: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                'Flutter Tips',
-                style: TextStyle(fontSize: 28),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return EditNoteView();
+        }));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.blue, borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24, bottom: 24, top: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  'Flutter Tips',
+                  style: TextStyle(fontSize: 28),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    'Build your career with Saif Abdelaziz.',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                trailing: Transform.translate(
+                  offset: Offset(0, -30),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        FontAwesomeIcons.trash,
+                        size: 24,
+                      )),
+                ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  'Build your career with Saif Abdelaziz.',
-                  style: TextStyle(fontSize: 18),
+                  'May 21,2021',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withValues(alpha: .5),
+                  ),
                 ),
               ),
-              trailing: Transform.translate(
-                offset: Offset(0, -30),
-                child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      FontAwesomeIcons.trash,
-                      size: 24,
-                    )),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                'May 21,2021',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black.withValues(alpha: .5),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
