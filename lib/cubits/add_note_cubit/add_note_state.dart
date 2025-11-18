2640d3675cbcf359a688ecce94d4c963b7012eda
saif <seif301x@gmail.com>
2025-11-18 13:18:14 +0200

    create add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,10 @@
+part of 'add_note_cubit.dart';
+
+abstract class AddNoteState extends Equatable {
+  const AddNoteState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AddNoteInitial extends AddNoteState {}
