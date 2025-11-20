72c4a4f0d5deaad2bd6fcc2a85e844f605c03554
saif <seif301x@gmail.com>
2025-11-20 13:45:41 +0200

    create notes state

--- lib/cubits/notes/notes_cubit.dart
+++ lib/cubits/notes/notes_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
--- lib/cubits/notes/notes_state.dart
+++ lib/cubits/notes/notes_state.dart
@@ -12,7 +12,9 @@ final class NotesInitial extends NotesState {}
 final class NotesLoading extends NotesState {}
 
 final class NotesSuccess extends NotesState {
-  
+  final List<NoteModel> notes; //* Store notes in list in case of success
+
+  const NotesSuccess(this.notes);
 }
 
 final class NotesFailure extends NotesState {
